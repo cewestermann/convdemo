49f66a651d36699eb073af131d3a40920fef9cc5
Christian Emil Westermann <cew@rooftop-analytics.com>
2024-10-01 20:53:36 +0200

    minor

--- src/convdemo.c
+++ src/convdemo.c
@@ -9,6 +9,10 @@
 global_variable bool running;
 global_variable BITMAPINFO bitmap_info;
 global_variable void* bitmap_memory;
+
+global_variable int bitmap_width;
+global_variable int bitmap_height;
+
 // A device context is a Windows term when working with the GDI, the Windows Graphics Device Interface
 // It's a place where you draw, so if you have several different device contexts, you're drawing multiple places,
 // such as multiple windows. 
@@ -16,8 +20,8 @@ global_variable void* bitmap_memory;
 internal void win32_resize_DIB_section(int width, int height) 
 {
   bitmap_info.bmiHeader.biSize = sizeof(bitmap_info.bmiHeader);
-  bitmap_info.bmiHeader.biWidth = width;
-  bitmap_info.bmiHeader.biHeight = height;
+  bitmap_info.bmiHeader.biWidth = bitmap_width;
+  bitmap_info.bmiHeader.biHeight = bitmap_height;
   bitmap_info.bmiHeader.biPlanes = 1;
   bitmap_info.bmiHeader.biBitCount = 32;
   bitmap_info.bmiHeader.biCompression = BI_RGB;
@@ -28,17 +32,28 @@ internal void win32_resize_DIB_section(int width, int height)
     };
   } 
 
+  bitmap_width = width;
+  bitmap_height = height;
+
   int bytes_per_pixel = 4;
-  int bitmap_memory_size = bytes_per_pixel * width * height;
+  int bitmap_memory_size = bytes_per_pixel * bitmap_width * bitmap_height;
+  // Returns memory in pages. Since we pass in 0 for the starting address of the memory (as we don't really care)
+  // Windows automatically rounds up to the next page boundary, so that we don't cross more pages than necessary.
+  // The bitmap_memory size we need is specifically on this laptop: 3518976 bytes or 3436 KiB. Windows uses
+  // 4 KiB pages as default, which means that we've allocated 859 pages and we can step through them in the
+  // memory view of a debugger and we can see that they are all zeroed out.
   bitmap_memory = VirtualAlloc(0, bitmap_memory_size, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
 
 }
 
-internal void win32_update_window(HDC device_context, int x, int y, int width, int height) 
+internal void win32_update_window(HDC device_context, RECT* client_rect)
 {
+  int window_width = client_rect->right - client_rect->left;
+  int window_height = client_rect->bottom - client_rect->top;
+  
   StretchDIBits(device_context,
-                x, y, width, height,
-                x, y, width, height,
+                0, 0, window_width, window_height,
+                0, 0, bitmap_width, bitmap_height,
                 bitmap_memory,
                 &bitmap_info,
                 DIB_RGB_COLORS, SRCCOPY);
@@ -82,12 +97,11 @@ LRESULT win32_main_window_callback(HWND window,
       {
         PAINTSTRUCT paint;
         HDC device_context = BeginPaint(window, &paint);
-        // Perform painting here
-        int x = paint.rcPaint.left;
-        int y = paint.rcPaint.top;
-        int width = paint.rcPaint.right - paint.rcPaint.left;
-        int height = paint.rcPaint.bottom - paint.rcPaint.top;
-        win32_update_window(device_context, x, y, width, height);
+
+        RECT client_rect;
+        GetClientRect(window, &client_rect);
+
+        win32_update_window(device_context, &client_rect);
 
         EndPaint(window, &paint);
       } break;
